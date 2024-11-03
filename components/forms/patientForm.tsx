7c50d76d65fd98e15e9707edb13de55eaab46225
Belinda1704 <b.larose@alustudent.com>
2024-11-03 02:47:04 +0200

    Updated changes

--- components/forms/patientForm.tsx
+++ components/forms/patientForm.tsx
@@ -5,10 +5,16 @@ import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { Button } from "@/components/ui/button"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import customFormField from "@/components/ui/customFormField"
+import { Input } from "../ui/input"
 
-export enum FormFieldTypes ={
-  INPUT='input'
+export enum FormFieldType {
+  INPUT = 'input',
+  TEXTAREA = 'textarea',
+  PHONE_INPUT = 'phoneinput',
+  CHECKBOX = 'checkbox',
+  DATE_PICKER ='datepicker',
+  SELECT = 'select',
+  SKELETON = 'skeleton',
 }
 
 const formSchema = z.object({
@@ -36,9 +42,15 @@ const PatientForm = () => {
           <p className="text-dark-700">Schedule your first appointment.</p>
         </section>
 
-        <customFromField 
+        <customFormField>
         fieldType={FormField.INPUT}
-        />
+          control={form.control}
+          name="name"
+          label="Full name"
+          placeholder="Jon Doe"
+          iconSrc="/public/assets//icons/user.svg"
+          iconAlt="user
+        </customFormField>
 
         <FormField
           control={form.control}
