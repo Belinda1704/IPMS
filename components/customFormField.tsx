03a87eb659ecb959641f52e2620c983418bcc85c
Belinda1704 <b.larose@alustudent.com>
2024-11-03 14:53:45 +0200

    updated

--- components/customFormField.tsx
+++ components/customFormField.tsx
@@ -11,6 +11,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Control } from "react-hook-form"
 import { FormFieldType } from "./forms/PatientForm"
+import Image from "next/image"
 
 interface CustomProps {
     control: Control<any>,
@@ -27,13 +28,34 @@ interface CustomProps {
     renderSkeleton?: (field: any) => React.ReactNode,
 }
 
-const RenderField = ({ field, props}: { field: any; props: CustomProps }) => {
-    return (
-        <Input
-            type="text"
-            placeholder="Jon Doe"
-        />
-    )
+const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
+    const { fieldType, iconSrc, iconAlt, placeholder } = props;
+
+    switch (props.fieldType) {
+        case FormFieldType.INPUT:
+            return (
+                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                    {props.iconSrc && (
+                        <Image
+                            src="iconSrc"
+                            height={24}
+                            width={24}
+                            alt={iconAlt || 'icon'}
+                            className="ml-2"
+                        />
+                    )}
+                    <FormControl>
+                        <Input
+                            placeholder={placeholder}
+                            {...field}
+                            className="shad-input border-0"
+                        />
+                    </FormControl>
+                </div>
+            )
+        default:
+            break;
+    }
 }
 
 const CustomFormField = (props: CustomProps) => {
@@ -48,7 +70,7 @@ const CustomFormField = (props: CustomProps) => {
                         <FormLabel>{label}</FormLabel>
                     )}
 
-                    <RenderField field={field} props={props}/>
+                    <RenderField field={field} props={props} />
 
                     <FormMessage className="shad-error" />
                 </FormItem>
